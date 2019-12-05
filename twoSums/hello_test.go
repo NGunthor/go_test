dce5771539aba34b45e05ececaecf86578d801e2
ngunthor <clfdf23@gmail.com>
2019-12-05 15:31:38 +0300

    twoSums fail tests

--- twoSums/hello_test.go
+++ twoSums/hello_test.go
@@ -12,19 +12,37 @@ type testStruct struct{
 }
 
 var tests = []testStruct {
-	{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+	{[]int{2, 7, 11, 15}, 9, []int{1, 1}},
 	{[]int{4, 8, 0, 7, 2, 5}, 7, []int{2, 3}},
 	{[]int{1, 4, 6, 7, 10, 47, 4, 6, 7, 10, 47, 4, 6, 7, 10, 47, 4, 6, 7, 10, 47, 4, 6, 7, 10, 47, 4, 6, 7, 10, 47, 4, 6, 7, 10, 47, 4, 6, 7, 10, 47, 4, 6, 7, 10, 47, 4, 6, 7, 10, 47, 4, 6, 7, 10, 47, 4, 6, 7, 10, 47, 4, 6, 7, 10, 47, 60}, 61, []int{0, 66}},
 	{[]int{4, 8, 1, 4}, 8, []int{0, 3}},
 }
 
 func TestTwoSum(t *testing.T) {
-	for _, el := range tests {
-		v := TwoSum(el.values, el.target)
-		if !reflect.DeepEqual(v, el.result) {
+	for _, test := range tests {
+		v := TwoSum(test.values, test.target)
+		if !reflect.DeepEqual(v, test.result) {
 			t.Error(
-				"For", el.values,
-				"expected", el.result,
+				"For", test.values,
+				"expected", test.result,
+				"got", v)
+		}
+
+	}
+}
+
+func TestTwoSumFail(t *testing.T) {
+	for _, test := range tests {
+		v := TwoSum(test.values, test.target)
+		if reflect.DeepEqual(v, test.result) {
+			t.Log(
+				"Success for", test.values,
+				"expected", test.result,
+				"got", v)
+		} else {
+			t.Failed()
+			t.Log("Fail for", test.values,
+				"expected", test.result,
 				"got", v)
 		}
 	}
