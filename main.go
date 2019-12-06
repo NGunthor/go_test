bd74da807c5c842cce0d291de3b9a4456fb5058f
ngunthor <clfdf23@gmail.com>
2019-12-06 19:31:56 +0300

    workerPool w/o tests

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-	"tests/set"
-)
-
 func main() {
-	s := set.NewSet()
-	s.Add(5, 3, 7, 1, 8)
-	fmt.Println(reflect.TypeOf(s))
-	fmt.Println(s.ToSlice())
+
 }
