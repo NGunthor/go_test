352dd2f73121882c848d3a8e7a66a040e31d2359
ngunthor <clfdf23@gmail.com>
2019-12-10 12:33:44 +0300

    .

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
 
+func main() {
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			fmt.Println(runtime.NumCPU())
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
+
+
--- mergeTwoSortedLists/mergeTwoSortedLists.go
+++ mergeTwoSortedLists/mergeTwoSortedLists.go
@@ -1,23 +1,25 @@
 package mergeTwoSortedLists
 
-func Merge(nums1 []int, m int, nums2 []int, n int) []int{
-	a := make([]int, 0)
-	nums1 = nums1[:m]
+func Merge(nums1 []int, m int, nums2 []int, n int) []int {
+	a := make([]int, m)
+	copy(a, nums1[:m])
 	nums2 = nums2[:n]
-	for len(nums1) > 0 && len(nums2) > 0 {
-		if nums1[0] > nums2[0] {
-			a = append(a, nums2[0])
+	i := 0
+	for len(a) > 0 && len(nums2) > 0 {
+		if a[0] > nums2[0] {
+			nums1[i] = nums2[0]
 			nums2 = nums2[1:]
 		} else {
-			a = append(a, nums1[0])
-			nums1 = nums1[1:]
+			nums1[i] = a[0]
+			a = a[1:]
 		}
-
+		i++
 	}
-	if len(nums1) == 0 {
-		a = append(a, nums2...)
+	nums1 = nums1[:i]
+	if len(a) == 0 {
+		nums1 = append(nums1, nums2...)
 	} else {
-		a = append(a, nums2...)
+		nums1 = append(nums1, a...)
 	}
-	return a
+	return nums1
 }
