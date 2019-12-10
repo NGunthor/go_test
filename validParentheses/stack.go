9fa85a7b8d725648e8714b5bd3989c72035ed8ea
ngunthor <clfdf23@gmail.com>
2019-12-10 14:40:46 +0300

    validParentheses

---
+++ validParentheses/stack.go
@@ -0,0 +1,37 @@
+package validParentheses
+
+type stack struct {
+	values []rune
+	size   int
+}
+
+func (s *stack) Push(r rune) {
+	s.values = append(s.values, r)
+	s.size++
+}
+
+func (s *stack) Pop() rune {
+	var out rune = 0
+	if s.size > 0 {
+		out = s.values[s.size-1]
+		s.values = s.values[:s.size-1]
+		s.size--
+	}
+	return out
+}
+
+func (s *stack) Len() int {
+	return s.size
+}
+
+func (s *stack) Peak() rune {
+	var out rune = 0
+	if s.size > 0 {
+		out = s.values[s.size-1]
+	}
+	return out
+}
+
+func NewStack() *stack {
+	return &stack{[]rune{}, 0}
+}
---
+++ validParentheses/validParentheses.go
@@ -0,0 +1,34 @@
+package validParentheses
+
+func IsValid(s string) bool {
+	stack := NewStack()
+	for _, char := range s {
+		if char == '(' || char == '[' || char == '{' {
+			stack.Push(char)
+		} else {
+			switch char {
+			case ')':
+				if stack.Peak() != '(' {
+					return false
+				}
+				stack.Pop()
+			case '}':
+				if stack.Peak() != '{' {
+					return false
+				}
+				stack.Pop()
+			case ']':
+				if stack.Peak() != '[' {
+					return false
+				}
+				stack.Pop()
+			default:
+				return false
+			}
+		}
+	}
+	if stack.Len() > 0 {
+		return false
+	}
+	return true
+}
