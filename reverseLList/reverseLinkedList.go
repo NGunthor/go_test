0b1d7b8598a38a79bda9c15c5a9ab9093d47b1a0
ngunthor <clfdf23@gmail.com>
2019-12-16 13:54:43 +0300

    reverseLList v1

---
+++ reverseLList/reverseLinkedList.go
@@ -0,0 +1,25 @@
+package reverseLList
+
+//A struct that represents a node of the List
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil  || head.Next == nil {
+		return head
+	}
+
+	cur := head.Next
+	prev := head
+
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	head.Next = nil
+	return prev
+}
