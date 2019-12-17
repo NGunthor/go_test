2c466f02939ea7f4eb0069f8d3349edfe8a9c2e8
ngunthor <clfdf23@gmail.com>
2019-12-17 12:35:26 +0300

    mediator v1

---
+++ patterns/mediator/account.go
@@ -0,0 +1,60 @@
+package mediator
+
+import (
+	"fmt"
+	"log"
+)
+
+type Account interface {
+	add(int)
+	withdraw(int)
+	Send(rune, int)
+	GetSum() int
+	SetMediator(Mediator)
+	Show()
+}
+
+type account struct {
+	sum int
+	mediator Mediator
+}
+
+func (a *account)add(amount int) {
+	if amount > 0 {
+		a.sum += amount
+	} else {
+		fmt.Println("incorrect amount")
+	}
+}
+
+func (a *account)withdraw(amount int) {
+	if amount > 0 || a.sum - amount < 0{
+		a.sum -= amount
+	} else {
+		fmt.Println("incorrect amount")
+	}
+}
+
+func (a *account)Send(id rune, amount int) {
+	if a.mediator == nil {
+		log.Println("mediator required")
+		return
+	}
+	a.mediator.Notify('A', amount)
+}
+
+func (a *account)GetSum() int {
+	return a.sum
+}
+
+func (a *account)SetMediator(mediator Mediator) {
+	a.mediator = mediator
+}
+
+func (a *account)Show() {
+	fmt.Println(a)
+}
+
+func NewAccount(amount int) Account {
+	return &account{sum: amount}
+}
---
+++ patterns/mediator/mediator.go
@@ -0,0 +1,46 @@
+package mediator
+
+import (
+	"log"
+)
+
+type Mediator interface {
+	Notify(id rune, amount int)
+}
+
+type mediator struct {
+	colleagueA Account
+	colleagueB Account
+}
+
+func (m *mediator)Notify(id rune, amount int){
+	if m.colleagueA == nil || m.colleagueB == nil {
+		log.Println("mediator required")
+		return
+	}
+	if id == 'A' {
+		if m.colleagueB.GetSum() - amount < 0 {
+			log.Println("insufficient funds B")
+		} else {
+			m.colleagueA.add(amount)
+			m.colleagueB.withdraw(amount)
+		}
+	}
+	if id == 'B' {
+		if m.colleagueA.GetSum() - amount < 0 {
+			log.Println("insufficient funds A")
+			return
+		} else {
+			m.colleagueB.add(amount)
+			m.colleagueA.withdraw(amount)
+		}
+	}
+}
+
+func NewMediator(a Account, b Account) Mediator {
+	return &mediator{
+		colleagueA: a,
+		colleagueB: b,
+	}
+}
+
