8bd7b309f9f4a44f7df7a57810fe1dcb9d8052e0
ngunthor <clfdf23@gmail.com>
2019-12-17 15:08:30 +0300

    fmt

--- intersect/main/main.go
+++ intersect/main/main.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println(intersect.Intersect([]int{4,9,5}, []int{9,4,9,8,4}))
-	fmt.Println(intersect.Intersect([]int{1,2,2,1}, []int{2,2}))
+	fmt.Println(intersect.Intersect([]int{4, 9, 5}, []int{9, 4, 9, 8, 4}))
+	fmt.Println(intersect.Intersect([]int{1, 2, 2, 1}, []int{2, 2}))
 }
