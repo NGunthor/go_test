26ba7705ca9f9a3d404930f6f773af1afc26baca
ngunthor <clfdf23@gmail.com>
2019-12-18 20:15:12 +0300

    number-of-islands

---
+++ cmd/number-of-islands/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	number_of_islands "github.com/NGunthor/go_test/pkg/leetcode/number-of-islands"
+)
+
+func main() {
+	fmt.Println(number_of_islands.NumIslands([][]byte{{'1','1','1','1','0'}, {'1','1','0','1','0'}, {'1','1','0','0','0'}, {'0','0','0','0','0'}}))
+}
---
+++ pkg/leetcode/number-of-islands/number_of_islands.go
@@ -0,0 +1,35 @@
+package number_of_islands
+
+// NumsIslands ...
+func NumIslands(grid [][]byte) int {
+	count := 0
+	for y := range grid {
+		for x := range grid[y] {
+			if grid[y][x] == 49 {
+				count++
+				alg(x, y, grid)
+			}
+		}
+	}
+	return count
+}
+
+func alg(x int, y int, grid [][]byte) {
+	h := len(grid)
+	l := len(grid[0])
+	if grid[y][x] == 49 {
+		grid[y][x] = 48
+		if x + 1 < l {
+			alg(x + 1, y, grid)
+		}
+		if x - 1 > -1 {
+			alg(x - 1, y, grid)
+		}
+		if y + 1 < h {
+			alg(x, y + 1, grid)
+		}
+		if y - 1 > -1 {
+			alg(x, y - 1, grid)
+		}
+	}
+}
