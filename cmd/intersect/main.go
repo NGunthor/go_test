ed3d352b64c6ffbf4a6017168ba06e575c8db4a4
ngunthor <clfdf23@gmail.com>
2020-01-15 12:26:44 +0300

    intersect convention

--- cmd/intersect/main.go
+++ cmd/intersect/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	intersect2 "github.com/NGunthor/go_test/pkg/leetcode/intersect"
+
+	"github.com/NGunthor/go_test/pkg/leetcode/intersect"
 )
 
 func main() {
-	fmt.Println(intersect2.Intersect([]int{4, 9, 5}, []int{9, 4, 9, 8, 4}))
-	fmt.Println(intersect2.Intersect([]int{1, 2, 2, 1}, []int{2, 2}))
+	numbers := intersect.NewNumbers([]int{1,2,3,4}, []int{2,3})
+	fmt.Println(numbers.Intersect())
 }
--- pkg/leetcode/intersect/intersect.go
+++ pkg/leetcode/intersect/intersect.go
@@ -1,17 +1,35 @@
 package intersect
 
+// Numbers provides interface for numbers struct
+type Numbers interface {
+	Intersect() []int
+}
+
+type numbers struct {
+	nums1 []int
+	nums2 []int
+}
+
 // Intersect main algorithm function
-func Intersect(nums1 []int, nums2 []int) []int {
+func (n *numbers) Intersect() []int {
 	m := map[int]int{}
-	for _, el := range nums1 {
+	for _, el := range n.nums1 {
 		m[el]++
 	}
 	out := make([]int, 0)
-	for _, el := range nums2 {
+	for _, el := range n.nums2 {
 		if m[el] > 0 {
 			m[el]--
 			out = append(out, el)
 		}
 	}
 	return out
+}
+
+// NewNumbers ...
+func NewNumbers(nums1, nums2 []int) Numbers {
+	return &numbers{
+		nums1: nums1,
+		nums2: nums2,
+	}
 }
